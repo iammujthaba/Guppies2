d0b37c316c6f0a641ae8c9c37fb2ee05d761a01e
mujthaba <iammujthaba@gmail.com>
2024-06-07 10:31:42 +0530

    just update

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
+app_name = 'auth_app'
 urlpatterns = [
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
